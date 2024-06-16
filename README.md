6098a8c6444a08afc6e3cff4022aa841541fa1c2
Eric C <39015447+ricetech@users.noreply.github.com>
2024-06-16 00:45:02 -0400

    chore: setup expo for tamagui

--- README.md
+++ README.md
@@ -22,6 +22,8 @@ In the output, you'll find options to open the app in a
 - [Android emulator](https://docs.expo.dev/workflow/android-studio-emulator/)
 - [iOS simulator](https://docs.expo.dev/workflow/ios-simulator/)
 - [Expo Go](https://expo.dev/go), a limited sandbox for trying out app development with Expo
+- **NOTE**: Support for Web is NOT enabled by default. Complete the commented steps in _layout.tsx to enable it,
+   if required.
 
 You can start developing by editing the files inside the **app** directory. This project uses [file-based routing](https://docs.expo.dev/router/introduction).
 
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,45 @@
-import { Stack } from "expo-router";
+// WEB SUPPORT: Do this https://tamagui.dev/docs/guides/metro#web-support
+// import '../tamagui-web.css'
+
+import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native'
+import {SplashScreen, Stack} from 'expo-router'
+import { useColorScheme } from 'react-native'
+import { TamaguiProvider } from 'tamagui'
+
+import { tamaguiConfig } from '@/tamagui.config'
+import {useFonts} from "expo-font";
+import {useEffect} from "react";
 
 export default function RootLayout() {
-  return (
-    <Stack>
-      <Stack.Screen name="index" />
-    </Stack>
-  );
+    const colorScheme = useColorScheme()
+
+    // Prevent the splash screen from auto-hiding before asset loading is complete.
+    SplashScreen.preventAutoHideAsync()
+
+    const [interLoaded, interError] = useFonts({
+        Inter: require('@tamagui/font-inter/otf/Inter-Medium.otf'),
+        InterBold: require('@tamagui/font-inter/otf/Inter-Bold.otf'),
+    })
+
+    useEffect(() => {
+        if (interLoaded || interError) {
+            // Hide the splash screen after the fonts have loaded (or an error was returned) and the UI is ready.
+            SplashScreen.hideAsync()
+        }
+    }, [interLoaded, interError])
+
+    if (!interLoaded && !interError) {
+        return null
+    }
+
+    return (
+        // add this
+        <TamaguiProvider config={tamaguiConfig} defaultTheme={colorScheme!}>
+            <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+                <Stack>
+                    <Stack.Screen name="index" />
+                </Stack>
+            </ThemeProvider>
+        </TamaguiProvider>
+    )
 }
