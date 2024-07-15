5276d9a49eecb422ef5cf8a92be967c3e3a51b05
Eric C <39015447+ricetech@users.noreply.github.com>
2024-07-15 05:03:17 -0400

    feat: add content to home page

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,16 @@
-import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { Image, StyleSheet, Text, View } from "react-native";
+
+import LogoImg from "@/assets/images/logo.png";
 
 export default function HomeScreen() {
   return (
     <View style={styles.default}>
-      <Text>Home Page</Text>
+      <Image source={LogoImg} alt="logo" style={styles.image} />
+      <Text style={styles.header}>Welcome to ShopPing!</Text>
+      <Text style={styles.subTitle}>
+        Please explore our app using the buttons below.
+      </Text>
     </View>
   );
 }
@@ -11,7 +18,20 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
   default: {
     flex: 1,
-    justifyContent: "center",
+    // justifyContent: "center",
     alignItems: "center",
+  },
+  header: {
+    fontSize: 32,
+    fontWeight: "700",
+  },
+  subTitle: {
+    marginTop: 15,
+    fontSize: 18,
+  },
+  image: {
+    width: "50%",
+    resizeMode: "contain",
+    marginTop: "20%",
   },
 });
